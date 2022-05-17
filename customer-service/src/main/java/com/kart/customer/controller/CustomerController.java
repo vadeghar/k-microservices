27d130f89d9a72a4c69ed6cfa0ccb8f2e94204d7
Lakshman Vadeghar <vadeghar@gmail.com>
2022-05-18 00:32:11 +0530

    Logs

--- customer-service/src/main/java/com/kart/customer/controller/CustomerController.java
+++ customer-service/src/main/java/com/kart/customer/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.kart.customer.controller;
 
 import com.kart.customer.entity.Customer;
 import com.kart.customer.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("customer")
+@Slf4j
 public class CustomerController {
     private CustomerService customerService;
     public CustomerController(CustomerService customerService) {
@@ -18,16 +20,19 @@ public class CustomerController {
 
     @PostMapping("/")
     public Customer save(@RequestBody Customer customer) {
+        log.info("Inside save of CustomerController");
         return customerService.save(customer);
     }
 
     @GetMapping("/")
     public List<Customer> getAllCustomers() {
+        log.info("Inside getAllCustomers of CustomerController");
         return customerService.getAll();
     }
 
     @GetMapping("/message")
     public String test() {
+        log.info("Inside test of CustomerController");
         return "Hello From Customer";
     }
 }
--- customer-service/src/main/java/com/kart/customer/service/CustomerService.java
+++ customer-service/src/main/java/com/kart/customer/service/CustomerService.java
@@ -2,11 +2,13 @@ package com.kart.customer.service;
 
 import com.kart.customer.dao.CustomerDAO;
 import com.kart.customer.entity.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class CustomerService {
     private CustomerDAO repository;
     public CustomerService(CustomerDAO repository) {
@@ -14,9 +16,11 @@ public class CustomerService {
     }
 
     public List<Customer> getAll() {
+        log.info("Inside getAll of CustomerService");
         return repository.findAll();
     }
     public Customer save(Customer customer) {
+        log.info("Inside save of CustomerService");
         return repository.save(customer);
     }
 }
